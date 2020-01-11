504860d4569985a467063e9a6dfd6bce083beefd
abp437 <abp437@gmail.com>
2020-01-11 22:02:04 +0530

    Add Swipe Deck Animation along with rotation

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,59 @@
 import React, { useState } from "react";
-import { Animated, PanResponder, ScrollView, Text, View } from "react-native";
+import {
+  Animated,
+  Dimensions,
+  PanResponder,
+  ScrollView,
+  Text,
+  View
+} from "react-native";
 import { Button, Card } from "react-native-elements";
 
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
 const renderCards = data =>
-  data.map(item => (
-    <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
-      <Text style={{ marginBottom: 10 }}>I can customize the card further</Text>
-      <Button
-        icon={{ name: "code", color: "#fff" }}
-        backgroundColor="#03a9f4"
-        title="View Now!"
-      />
-    </Card>
-  ));
+  data.map((item, index) => {
+    if (index === 0) {
+      return (
+        <Animated.View
+          key={item.id}
+          style={getCardStyle()}
+          {...panResponder.panHandlers}
+        >
+          {getCardItem(item)}
+        </Animated.View>
+      );
+    }
+    return getCardItem(item);
+  });
+
+const getCardStyle = () => {
+  // Here we are establishing a linear relation between
+  // the amount of pixels a user drags the card and
+  // how much should the card rotate according to it
+  // This is interpolation.
+  // We are using SCREEN_WIDTH here because we want it to work on different devices
+  // We could have used any integer otherwise
+  const rotate = cardPosition.x.interpolate({
+    inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+    outputRange: ["-120deg", "0deg", "120deg"]
+  });
+  return {
+    ...cardPosition.getLayout(),
+    transform: [{ rotate }]
+  };
+};
+
+const getCardItem = item => (
+  <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+    <Text style={{ marginBottom: 10 }}>I can customize the card further</Text>
+    <Button
+      icon={{ name: "code", color: "#fff" }}
+      backgroundColor="#03a9f4"
+      title="View Now!"
+    />
+  </Card>
+);
 
 const cardPosition = new Animated.ValueXY();
 
@@ -30,14 +71,7 @@ const panResponder = PanResponder.create({
 });
 
 const Deck = ({ data }) => {
-  return (
-    <Animated.View
-      style={cardPosition.getLayout()}
-      {...panResponder.panHandlers}
-    >
-      {renderCards(data)}
-    </Animated.View>
-  );
+  return <View>{renderCards(data)}</View>;
 };
 
 export default Deck;
